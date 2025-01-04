f1b60147e884d4966b2d08eb5198c065d7e29182
manvm <info.manvm@gmail.com>
2025-01-04 18:51:00 +0530

    Deploy website - based on 9d4eef8ab293ffdf7fff857c4a3a67983ec51755

---
+++ assets/js/36994c47.283d8952.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_school=self.webpackChunkmy_school||[]).push([[9858],{5516:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
--- assets/js/9292cc2d.6b74569e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_school=self.webpackChunkmy_school||[]).push([[1037],{5745:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
